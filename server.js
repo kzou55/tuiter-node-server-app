1b16d32921c1184926baa6ea1ef5886aae619d37
Ken Zou <kzou55@gmail.com>
2022-11-16 18:41:06 -0500

    changed app name to server

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-TuitsController(app);
-HelloController(app);
-UserController(app);
-app.listen(process.env.PORT || 4000);
+const server = express();
+server.use(cors());
+server.use(express.json());
+TuitsController(server);
+HelloController(server);
+UserController(server);
+server.listen(process.env.PORT || 4000);
